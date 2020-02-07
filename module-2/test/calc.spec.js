b190f02bc4252ca9d0f055780d29649145428c3e
Sandeep Neela <sandeep_neela@epam.com>
2020-02-07 17:50:04 +0530

    commiting task

--- module-2/test/calc.spec.js
+++ module-2/test/calc.spec.js
@@ -20,5 +20,79 @@ describe.only('calc', () => {
     it('should display proper inpuy',()=>{
         expect(calc(3).v).to.equal(3);
     });
+    it('should should give  result 8',()=>{
+        //given
+        const c=calc(3);
+        //when
+        c.add(5);
+        //then
+        expect(c.v).to.equal(8);
+    });
+    it('should should give  result 1',()=>{
+        //given
+        const c=calc(3);
+        //when
+        c.minus(2);
+        //then
+        expect(c.v).to.equal(1);
+    });
+    it('should should give  result 2',()=>{
+        //given
+        const c=calc(4);
+        //when
+        c.sqrt();
+        //then
+        expect(c.v).to.equal(2);
+    });
+    it('should should give  result 30 by performing *',()=>{
+        //given
+        const c=calc(3);
+        //when
+        c.times(10);
+        //then
+        expect(c.v).to.equal(30);
+    });
+    it('should should give  result 5 by performing division',()=>{
+        //given
+        const c=calc(10);
+        //when
+        c.divide(2);
+        //then
+        expect(c.v).to.equal(5);
+    });
+    it('should should give  result 0 by performing modulo',()=>{
+        //given
+        const c=calc(10);
+        //when
+        c.modulo(5);
+        //then
+        expect(c.v).to.equal(0);
+    });
+    it('should should give  error by performing division',()=>{
+        //given
+        const c=calc(10);
+        //when +then
+        
+        //then
+        expect(()=>c.divide(0)).to.throw;
+    });
+    it('should should give  error by performing sqrt',()=>{
+        //given
+        const c=calc(-3);
+        //when +then
+        
+        //then
+        expect(()=>c.sqrt(2)).to.throw;
+    });
+    it('should should give  result 24 by performing operations',()=>{
+        //given
+        const c=calc(3);
+        //when +then
+        c.add(7).minus(3).times(4);
+        
+        //then
+        expect(c.v).to.equal(24);
+    });
+
 
 });
